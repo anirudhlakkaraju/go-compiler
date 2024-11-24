cad95be55fd4f7c54980cb47bbccf21973971dd8
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-24 16:05:09 -0600

    feat: vm definition and implementation

---
+++ src/monkey/vm/vm.go
@@ -0,0 +1,68 @@
+package vm
+
+import (
+	"fmt"
+	"go-compiler/src/monkey/code"
+	"go-compiler/src/monkey/compiler"
+	"go-compiler/src/monkey/object"
+)
+
+const StackSize = 2048
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+	sp    int // Always points to the next value. Top of stack is stack[sp-1]
+}
+
+func New(byteCode *compiler.Bytecode) *VM {
+	return &VM{
+		constants:    byteCode.Constants,
+		instructions: byteCode.Instructions,
+		stack:        make([]object.Object, StackSize),
+		sp:           0,
+	}
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+// Run fetches, decodes and executes VM instructions
+func (vm *VM) Run() error {
+	// Iterate through the instructions using an instruction pointer
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+
+		switch op {
+		case code.OpConstant:
+			// read constant index in the constant pool
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// push objects onto VM call stack
+func (vm *VM) push(obj object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = obj
+	vm.sp += 1
+
+	return nil
+}
