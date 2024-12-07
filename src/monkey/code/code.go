990813ce665a0cbd0cfc9233faebadf4902cfd9b
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-12-07 17:15:27 +0530

    feat: Set and Get opcodes for var assignment

--- src/monkey/code/code.go
+++ src/monkey/code/code.go
@@ -28,6 +28,8 @@ const (
 	OpJumpNotTruthy
 	OpJump
 	OpNull
+	OpGetGlobal
+	OpSetGlobal
 )
 
 // Definition provides a readable name for the Opcode and number of bytes each operand takes up
@@ -53,6 +55,8 @@ var definitions = map[Opcode]*Definition{
 	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
 	OpJump:          {"OpJump", []int{2}},
 	OpNull:          {"OpNull", []int{}},
+	OpGetGlobal:     {"OpGetGlobal", []int{2}},
+	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 }
 
 // Lookup returns the definition of operation
