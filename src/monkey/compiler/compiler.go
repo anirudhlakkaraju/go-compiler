25591fcd4f3f5f5aa4b0d919db7a63c8d38f1804
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-12-17 15:27:51 +0530

    feat: compile hash literals

--- src/monkey/compiler/compiler.go
+++ src/monkey/compiler/compiler.go
@@ -5,6 +5,7 @@ import (
 	"go-compiler/src/monkey/ast"
 	"go-compiler/src/monkey/code"
 	"go-compiler/src/monkey/object"
+	"sort"
 )
 
 type Bytecode struct {
@@ -220,6 +221,30 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 		c.emit(code.OpArray, len(node.Elements))
+
+	case *ast.HashLiteral:
+		keys := []ast.Expression{}
+		for k := range node.Pairs {
+			keys = append(keys, k)
+		}
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i].String() < keys[j].String()
+		})
+
+		for _, k := range keys {
+			// Compile the keys first
+			err := c.Compile(k)
+			if err != nil {
+				return err
+			}
+
+			// Then compile the corresponding values
+			err = c.Compile(node.Pairs[k])
+			if err != nil {
+				return err
+			}
+		}
+		c.emit(code.OpHash, len(node.Pairs)*2)
 	}
 
 	return nil
