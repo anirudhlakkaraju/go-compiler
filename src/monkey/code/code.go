45d79138a9fc06866556fa99836a556f022657c2
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-03-09 19:11:35 -0500

    feat: opcodes for calling, implicit and explicit return from functions

--- src/monkey/code/code.go
+++ src/monkey/code/code.go
@@ -33,6 +33,9 @@ const (
 	OpArray
 	OpHash
 	OpIndex
+	OpCall
+	OpReturnValue
+	OpReturn
 )
 
 // Definition provides a readable name for the Opcode and number of bytes each operand takes up
@@ -63,6 +66,9 @@ var definitions = map[Opcode]*Definition{
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
 	OpIndex:         {"OpIndex", []int{}},
+	OpCall:          {"OpCall", []int{}},
+	OpReturnValue:   {"OpReturnValue", []int{}},
+	OpReturn:        {"OpReturn", []int{}},
 }
 
 // Lookup returns the definition of operation
